dab9e52d0d792ac81ec44bb06f2c72e1c8414808
Majoie <20mk321@esisalama.org>
2026-08-27 17:42:45 +0200

    ajout du fichier js

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
+
 
 app_name = 'projects'
 
 urlpatterns = [
+    path(
+        '.well-known/assetlinks.json', 
+        TemplateView.as_view(
+            template_name="assetlinks.json", 
+            content_type="application/json"  # Très important pour que Google le reconnaisse
+        ), 
+        name='assetlinks'
+    ),
+    
     path('', views.home, name='home'),
     path('create/', views.create_project, name='create'),
     path('project/<int:pk>/', views.project_detail, name='detail'),
